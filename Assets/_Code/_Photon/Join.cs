8ffa41332e1cc22a398915c11ee07fc64c7c4d47
Denys Bachynskyi <denys.bachinskiy@gmail.com>
2025-07-11 12:03:49 +0300

    rooms change, undone

--- Assets/_Code/_Photon/Join.cs
+++ Assets/_Code/_Photon/Join.cs
@@ -1,12 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
 namespace _Code._Photon
 {
-  public class Join : MonoBehaviourPunCallbacks
-  {
-    public void JoinRoom()
+    public class Join : MonoBehaviourPunCallbacks
     {
-      PhotonNetwork.JoinRandomRoom();
+        [Header("UI")]
+        public GameObject joinLobby;
+        public Button joinRoomPrefab;
+
+        private void Start()
+        {
+            joinLobby.active = false;
+        }
+
+        public override void OnRoomListUpdate(List<RoomInfo> roomList)
+        {
+            for (int i = 0; i < roomList.Count; i++)
+            {
+                Debug.Log($"Room Name: {roomList[i].Name}, Max Players: {roomList[i].MaxPlayers}");
+            }
+        }
+
+        public void JoinRoom()
+        {
+            
+        }
     }
-  }
-}
+}   
